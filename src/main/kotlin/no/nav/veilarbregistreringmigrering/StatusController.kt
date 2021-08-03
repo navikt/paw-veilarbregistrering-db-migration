cda52c6ec9626dd19978b5c55ec8e59f6f75f3b2
Michael Gundersen <21148282+theagilepadawan@users.noreply.github.com>
2021-08-03 14:03:29 +0200

    F!! Oppdaterer til korremt prometheus path og legger til heartbeat-endepunkter

---
+++ src/main/kotlin/no/nav/veilarbregistreringmigrering/StatusController.kt
@@ -0,0 +1,18 @@
+package no.nav.veilarbregistreringmigrering
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping
+class StatusController {
+
+    @GetMapping("/isready")
+    fun isReady() = ResponseEntity.ok("OK")
+
+    @GetMapping("/isalive")
+    fun isHealthy() = ResponseEntity.ok("OK")
+
+}
